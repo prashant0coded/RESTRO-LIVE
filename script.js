72785903b44841c60b27a6714e5a9c9b14575223
prashant0coded <prashantrajput01234@gmail.com>
2024-12-17 11:26:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+let menu = document.querySelector('#menu-bars'); let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => { menu.classList.toggle('fa-times'); navbar.classList.toggle('active'); };
+
+window.onscroll = () => { menu.classList.remove('fa-times'); navbar.classList.remove('active'); };
+
+document.querySelector('#search-icon').onclick = () => { document.querySelector('#search-form').classList.toggle('active'); };
+
+document.querySelector('#close').onclick = () => { document.querySelector('#search-form').classList.remove('active'); };
+
+var swiper = new Swiper(".home-slider", { spaceBetween: 30, centeredSlides: true, autoplay: { delay: 7500, disableOnInteraction: false, }, pagination: { el: ".swiper-pagination", clickable: true, }, loop:true, });
+
+var swiper = new Swiper(".review-slider", { spaceBetween: 20, centeredSlides: true, autoplay: { delay: 7500, disableOnInteraction: false, }, loop:true, breakpoints:{ 0:{ slidePerView:1, }, 640:{ slidePerView:2, }, 768:{ slidePerView:3 }, }, });
